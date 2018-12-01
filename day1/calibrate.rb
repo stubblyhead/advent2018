7fe791d3f2bbee544251f994479cb2aa1b3319aa
Mike Stevenson <stubblyhead@gmail.com>
2018-12-01 09:52:34 -0800

    can probably do this faster but whatever

--- day1/calibrate.rb
+++ day1/calibrate.rb
@@ -1,5 +1,19 @@
 lines = File.readlines('./input', :chomp=> true)
 freq = 0
-lines.each { |i| freq += i.to_i }
+freq_values = []
+lines.each do |i|
+  freq += i.to_i
+  freq_values.push(freq)
+end
 
-puts "calibrated frequency is #{freq}"
+puts "part 1 calibrated frequency is #{freq}"
+
+lines.cycle do |i|
+  freq += i.to_i
+  if freq_values.index(freq)
+    puts "part 2 calibrated frequency is #{freq}"
+    break
+  else
+    freq_values.push(freq)
+  end
+end
