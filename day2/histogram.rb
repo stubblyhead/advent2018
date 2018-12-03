6068d8892f485973205d62135c72824298188056
Mike Stevenson <stubblyhead@gmail.com>
2018-12-03 08:43:40 -0800

    on the right track for part two, one-off fuction is not quite right though

--- day2/histogram.rb
+++ day2/histogram.rb
@@ -11,3 +11,20 @@ box_ids.each do |i|
 end
 
 puts "part one checksum is #{two_counts * three_counts}"
+
+def one_off?(x, y)
+  different_chars = 0
+  (0..x.length - 1).each { |i| different_chars += 1 if x[i] != y[i] }
+  different_chars == 1 ? true : false
+end
+
+(0..box_ids.length - 2).each do |i|
+  (i + 1..box_ids.length - 1).each do |j|
+    if one_off?(box_ids[i], box_ids[j])
+      common_chars = (box_ids[i].chars & box_ids[j].chars).join
+      puts "common characters are #{common_chars}"
+      exit
+    end
+  end
+
+end
