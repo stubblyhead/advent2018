cb7d2cb120b082f6f90e12bed1a6ba6b61f1e0d0
Mike Stevenson <stubblyhead@gmail.com>
2018-12-03 10:49:13 -0800

    previous commit assumed that no duplicates had the same top corner, which is not actually in the spec, we can actually do the whole thing line by line without keeping it in a hash to loop through a second time

--- day3/quiltingbee.rb
+++ day3/quiltingbee.rb
@@ -1,15 +1,10 @@
 grid = Array.new(1050) { Array.new(1050) { 0 } }
 
-claims = {}
-
-lines = File.readlines('./testcase', :chomp => true)
+lines = File.readlines('./input', :chomp => true)
 lines.each do |i|
   parts = i.split(/@|:/)
-  top_corner = parts[1].split(',').map { |i| i.to_i }
-  claims[top_corner] = parts[2].split('x').map { |i| i.to_i }
-end
-
-claims.each do |corner, size|
+  corner = parts[1].split(',').map { |i| i.to_i }
+  size = parts[2].split('x').map { |i| i.to_i }
   (corner[0]..corner[0]+size[0]-1).each do |i|
     (corner[1]..corner[1]+size[1]-1).each do |j|
       grid[i][j] += 1
