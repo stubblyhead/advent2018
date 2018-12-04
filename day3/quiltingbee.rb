32860925f04e018392a4be9bf021ff1ed910dfe2
Mike Stevenson <stubblyhead@gmail.com>
2018-12-03 20:42:50 -0800

    part 2 passing testcase

--- day3/quiltingbee.rb
+++ day3/quiltingbee.rb
@@ -1,10 +1,14 @@
 grid = Array.new(1050) { Array.new(1050) { 0 } }
 
-lines = File.readlines('./input', :chomp => true)
+claims = {} #it doesn't matter that multiple claims can start at the same point
+            #because if they do they're automatically disqualified from being the
+            #only non-overlapping claim
+lines = File.readlines('./testcase', :chomp => true)
 lines.each do |i|
   parts = i.split(/@|:/)
   corner = parts[1].split(',').map { |i| i.to_i }
   size = parts[2].split('x').map { |i| i.to_i }
+  claims[corner] = [size, parts[0]]
   (corner[0]..corner[0]+size[0]-1).each do |i|
     (corner[1]..corner[1]+size[1]-1).each do |j|
       grid[i][j] += 1
@@ -18,3 +22,17 @@ grid.each do |i|
 end
 
 puts "#{overlap_count} square inches overlap"
+
+claims.each do |corner, size|
+  area_sum = 0
+  goal = size[0][0] * size[0][1]
+  (corner[0]..corner[0] + size[0][0] - 1).each do |i|
+    (corner[1]..corner[1] + size[0][1] - 1).each do |j|
+      area_sum += grid[i][j]
+    end
+  end
+  if goal == area_sum
+    puts "non-overlapping claim is #{size[1]}"
+    exit
+  end
+end
