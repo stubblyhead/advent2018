b1fece0a7f17e2a6eb52a3d9a6fcc99034905e14
Mike Stevenson <stubblyhead@gmail.com>
2018-12-14 22:45:13 -0800

    test case passing

---
+++ day5/plastics.rb
@@ -0,0 +1,28 @@
+require 'pry'
+binding.pry
+
+
+class Polymer
+  def initialize(str)
+    @chain = str
+  end
+
+  def react
+    (0..@chain.length-2).each do |i|
+      if @chain[i] == @chain[i+1].swapcase
+        @chain = @chain[0,i] + @chain[i+2..-1]
+        react
+        break
+      end
+    end
+  end
+
+  def to_s
+    @chain
+  end
+end
+
+my_polymer = Polymer.new(File.read('./testcase', :chomp => true))
+
+my_polymer.react
+puts my_polymer
