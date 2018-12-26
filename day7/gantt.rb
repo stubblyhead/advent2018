4a1f97255ffaead0f304c6bcfa40b9b5be7e0afc
Mike Stevenson <stubblyhead@gmail.com>
2018-12-25 17:02:18 -0800

    thanks rubber ducky

--- day7/gantt.rb
+++ day7/gantt.rb
@@ -1,14 +1,13 @@
-# require 'pry'
-# binding.pry
-
 class Task
-  attr_reader :name, :dependencies, :requirements, :is_complete
+  attr_reader :name, :dependencies, :requirements, :is_complete, :is_active, :duration
 
   def initialize(name)
     @name = name
     @dependencies = []
     @requirements = []
     @is_complete = false
+    @is_active = false
+    @duration = name.bytes[0] - 4
   end
 
   def add_dependency(task)
@@ -23,6 +22,12 @@ class Task
     @requirements.delete(task)
   end
 
+  def work
+    @is_active = true
+    @duration -= 1
+    complete if @duration == 0
+  end
+
   def complete
     @is_complete = true
     @dependencies.each { |i| i.remove_requirement(self) }
@@ -32,10 +37,45 @@ class Task
     @name
   end
 
+  def inspect
+    to_s
+  end
+
   def <=>(other)
     self.name <=> other.name
   end
 end
+
+class Worker
+  attr_reader :task
+
+  def initialize
+    @task = nil
+  end
+
+  def assign(task)
+    @task = task
+  end
+
+  def work
+    return if @task == nil
+    @task.work
+    if @task.is_complete
+      @task = nil
+      return :finished
+    end
+  end
+
+  def is_busy?
+    if @task
+      return true
+    else
+      return false
+    end
+  end
+
+end
+
 
 tasklist = File.readlines('./input', :chomp => true)
 
@@ -51,13 +91,20 @@ tasklist.each do |i|
 end
 
 available_tasks = []
-task_order = ''
+workers = Array.new(5) { Worker.new }
+duration = 0
+
 while true
-  task_hash.each_value { |i| available_tasks.push(i) if i.requirements == [] and i.is_complete == false }
-  break if available_tasks.length == 0
-  available_tasks.sort!.uniq!
-  task_order += available_tasks[0].name
-  task_hash[available_tasks[0].name].complete
-  available_tasks.shift
+  if workers.select { |i| !i.is_busy? } # there's at least one available worker, so try to assign tasks
+    task_hash.each_value { |i| available_tasks.push(i) if i.requirements == [] and i.is_complete == false and i.is_active == false } # push tasks onto queue if they have no no prerequisites and have not been completed and are not currently being worked
+    available_tasks.sort!.uniq! # alphabetize and remove dupes
+    workers.each do |i|
+      i.assign(available_tasks.shift) unless i.is_busy?  # any available workers have been assigned any available tasks
+    end
+  end
+  workers.each { |i| i.work } # everybody works
+  duration += 1 # increment duration counter
+  break if task_hash.values.select { |i| i.is_complete }.length == task_hash.values.length # exit loop if all tasks are complete
 end
-puts task_order
+
+puts duration
